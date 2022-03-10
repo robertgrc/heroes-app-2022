5b623fe59cb1b94f5d775a3143d9a1351612309d
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-03-10 12:33:55 -0400

    create router

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DcScreen } from "../components/dc/DcScreen";
+import { LoginScreen } from "../components/login/LoginScreen";
+import { MarvelScreen } from "../components/marvel/MarvelScreen";
+import { SearchScreen } from "../components/search/SearchScreen";
+import { Navbar } from "../components/ui/Navbar";
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MarvelScreen />} />
+        <Route path="/marvel" element={<MarvelScreen />} />
+        <Route path="/dc" element={<DcScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
