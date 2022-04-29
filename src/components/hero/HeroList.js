2d0b4f74a12c8c42a3fc8c9c1ecd82fec90028ad
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-04-28 21:12:43 -0400

    hero information cards

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { getHeroesByPublisher } from "../selectors/getHeroByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <>
-      <h1>Hero List - {publisher}</h1>
-      <ul>
-        {heroes.map((hero) => (
-          <li key={hero.id}>{hero.superhero}</li>
-        ))}
-      </ul>
-    </>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
   );
 };
---
+++ src/components/selectors/getHeroByAlterEgo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const getHeroByAlterEgo = () => {
+  return <div>getHeroByAlterEgo</div>;
+};
